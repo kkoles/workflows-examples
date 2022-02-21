e8663be59ae892129b6f7a76e8458ebcce4557ba
Kseniya Kalesnikava <ksup4ik@gmail.com>
2022-02-21 17:43:27 +0300

    Testing working directory parameter

---
+++ terraform.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  region  = "eu-central-1"
+}
+
+resource "aws_instance" "web_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleWebServerInstance"
+  }
+}
