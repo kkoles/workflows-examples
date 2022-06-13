48dc86a0391978c58f4f01380a5c777b71bd2f6c
Kseniya Kalesnikava <ksup4ik@gmail.com>
2022-06-14 00:34:00 +0300

    testing python workflow

---
+++ scripts/script.py
@@ -0,0 +1,12 @@
+number_of_days = int(sys.argv[1])
+logs_s3_bucket = str(sys.argv[2])
+output_file    = str(sys.argv[3])
+user = str(sys.argv[4])
+
+if __name__ == "__main__":
+    print(f"""
+    NUMBER OF DAYS: {number_of_days}
+    LOGS BUCKET: {logs_s3_bucket}
+    OUTPUT_FILE: {output_file}
+    USER: {user}
+    """)
