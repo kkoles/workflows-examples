03edd29e7f3f199359d8e234bdb7119389870aa1
Kseniya Kalesnikava <ksup4ik@gmail.com>
2022-06-14 01:31:13 +0300

    Testing variable access

--- scripts/script.py
+++ scripts/script.py
@@ -7,16 +7,24 @@ import boto3
 import botocore
 import datetime
 import sys
+import os
+
 
 number_of_days = int(sys.argv[1])
 logs_s3_bucket = str(sys.argv[2])
 output_file    = str(sys.argv[3])
 user = str(sys.argv[4])
 
+# Get environment variables
+USER = os.getenv('USER')
+PASSWORD = os.getenv('PASSWORD')
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_S3_LOGS_BUCKET = os.getenv('AWS_S3_LOGS_BUCKET')
+
 if __name__ == "__main__":
     print(f"""
     NUMBER OF DAYS: {number_of_days}
-    LOGS BUCKET: {logs_s3_bucket}
-    OUTPUT_FILE: {output_file}
-    USER: {user}
+    LOGS BUCKET: {AWS_S3_LOGS_BUCKET}
+    USER: {USER}
+    AWS_ACCESS_KEY_ID: {AWS_ACCESS_KEY_ID }
     """)
